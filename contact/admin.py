212adf93d4c106ef5ad9535bfa45b88a879d6818
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-03-08 08:45:31 -0300

    Create template table contacts

--- contact/admin.py
+++ contact/admin.py
@@ -3,10 +3,12 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display=('id', 'first_name', 'last_name', 'created_date')
+    list_display=('id', 'first_name', 'last_name', 'created_date', 'show')
     list_display_links=('id', 'first_name',)
     search_fields=('first_name', 'last_name',)
-    ordering=('id',)
+    list_editable=('show',)
+    list_per_page=10
+    ordering=('-id',)
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
