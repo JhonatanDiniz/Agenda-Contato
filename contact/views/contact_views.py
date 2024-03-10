e4654aae58245fad8ec3beb3bb1aea63fb860626
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-03-10 14:34:10 -0300

    Create partial header

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
-    context = {
-        'contacts': contacts,
+    context ={
+        'contacts': contacts
     }
 
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact.objects, pk=contact_id, show=True)
+
+    context = {
+        'contacts': single_contact
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
