8befeac3d9b4cf9bcc9c9200dacb7976fe9dc8f2
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-03-11 07:34:58 -0300

    Added search functionality

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from contact.models import Contact
+from django.db.models import Q
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
+    context ={
+        'contacts': contacts
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+def search(request):
+    result_search = request.GET.get('q', '').strip()
+    
+    if result_search == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(Q(first_name__icontains=result_search) | 
+                Q(last_name__icontains=result_search) | 
+                Q(email__icontains=result_search) | 
+                Q(phone__icontains=result_search)
+        ).order_by('-id')
     context ={
         'contacts': contacts
     }
